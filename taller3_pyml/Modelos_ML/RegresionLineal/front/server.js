89463e2bb51831e6e5aaba30a9d1579c84c71df1
Juan Jose Cortes <juanjocortes518@gmail.com>
2026-09-11 14:46:57 -0500

    fix: estandarizar puerto 8080 para Railway en front y back

--- taller3_pyml/Modelos_ML/RegresionLineal/front/server.js
+++ taller3_pyml/Modelos_ML/RegresionLineal/front/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const mimeTypes = {
   '.html': 'text/html; charset=utf-8',
   '.css': 'text/css; charset=utf-8',
